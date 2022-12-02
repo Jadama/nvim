391fb82ada1eb83088641747cb481dbebd380dcf
Romano Zumbé <rzumbe@gmail.com>
2022-12-02 10:26:04 +0100

    Update harpoon.lua

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,15 +3,4 @@ if not status_ok then
 	return
 end
 telescope.load_extension("harpoon")
-local path = require("plenary.path")
 
-local stat, harpoon = pcall(require, "harpoon")
-if not stat then
-	print("harpoon not found")
-	return
-end
-harpoon.setup({
-	shortenPath = function(file)
-		return path.new(string.format("%s", file)):shorten(1, { -2, -1 })
-	end,
-})
