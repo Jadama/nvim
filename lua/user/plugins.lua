c211ac67f518d7ebfe46c5a38e98d9fa89c2470e
Romano Zumbé <rzumbe@gmail.com>
2022-12-02 14:04:02 +0100

    added trouble

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -154,6 +154,17 @@ local plugins = {
 	["nvim-treesitter/nvim-treesitter-textobjects"] = {
 		after = "nvim-treesitter",
 	},
+  
+    ["folke/trouble.nvim"] = {
+  requires = "kyazdani42/nvim-web-devicons",
+  config = function()
+    require("trouble").setup {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  end
+  },
 
 	-- Auto closing
 	["windwp/nvim-autopairs"] = {
